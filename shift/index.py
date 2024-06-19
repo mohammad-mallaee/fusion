9c89e7622b17ffaea4fa5db1012920a7956c622c
mohammad-mallaee <mohammad.mallaee@gmail.com>
2024-06-19 18:40:10 +0330

    merge pull and push functions to reduce inconsistencies

--- shift/index.py
+++ shift/index.py
@@ -10,9 +10,34 @@ from shift.helpers.constants import PULL, PUSH
 
 
 def shift(device: Device, ui: UserInterface, args):
+    command = args.command
+
+    if command not in [PULL, PUSH]:
+        raise Exception("Unknown Command !!")
+
+    source = args.source
+    storage = Storage()
+
+    if args.is_file:
+        if command == PULL:
+            progress = Progress(source.size, 1)
+            file = device.get_file(source)
+            device.pull_files(storage, progress, file)
+        elif command == PUSH:
+            progress = Progress(source.size, 1)
+            file = storage.get_file(source)
+            device.push_files(progress, file)
+    else:
+        return _shift_directory(device, storage, args, ui)
+
+
+def _shift_directory(device, storage, args, ui):
     source = args.source
     command = args.command
     destination = args.destination
+
+    source_interface = device if command == PULL else storage
+    dest_interface = storage if command == PULL else device
 
     def get_dest_path(path):
         return path.replace(source, destination)
@@ -20,59 +45,32 @@ def shift(device: Device, ui: UserInterface, args):
     def get_source_path(path):
         return path.replace(destination, source)
 
-    storage = Storage()
-    if command == PULL:
-        if args.is_file:
-            progress = Progress(source.size, 1)
-            file = device.get_file(source)
-            device.pull_files(storage, progress, file)
-        else:
-            file_listing = SyncList(
-                get_dest_path,
-                None if args.force else storage.should_sync,
-            )
-            ui.show(file_listing.window)
-            device.list_files(source, file_listing)
-            if args.dryrun:
-                return file_listing.show_result(ui)
-            progress = Progress(file_listing.transfer_size, file_listing.valid)
-            ui.replace_current(progress.window)
-            progress.start()
-            device.pull_files(storage, progress, *file_listing.files)
-            if args.delete:
-                delete_listing = DeleteList(get_source_path, device.should_delete)
-                ui.replace_current(delete_listing.window)
-                storage.list_files(destination, delete_listing)
-                storage.delete_files(delete_listing)
-                delete_listing.show_result(ui)
-            # ui.replace_current(progress.window)
-            # progress.end(ui)
+    file_listing = SyncList(
+        get_dest_path,
+        None if args.force else dest_interface.should_sync,
+    )
+    ui.show(file_listing.window)
+    source_interface.list_files(source, file_listing)
+    if args.dryrun:
+        return file_listing.show_result(ui)
 
+    delete_listing = None
+
+    def progress_callback(x):
+        ui.remove(x)
+        if delete_listing:
+            ui.show(delete_listing.window)
+
+    progress = Progress(file_listing.transfer_size, file_listing.valid)
+    ui.replace_current(progress.window)
+    progress.start()
+    if command == PULL:
+        device.pull_files(storage, progress, *file_listing.files)
     elif command == PUSH:
-        if args.is_file:
-            progress = Progress(source.size, 1)
-            file = storage.get_file(source)
-            device.push_files(progress, file)
-        else:
-            file_listing = SyncList(
-                get_dest_path,
-                None if args.force else storage.should_sync,
-            )
-            ui.show(file_listing.window)
-            storage.list_files(source, file_listing)
-            if args.dryrun:
-                return file_listing.show_result(ui)
-            progress = Progress(file_listing.transfer_size, file_listing.valid)
-            ui.replace_current(progress.window)
-            progress.start()
-            device.push_files(progress, *file_listing.files)
-            if args.delete:
-                delete_listing = DeleteList(get_source_path, storage.should_delete)
-                ui.replace_current(delete_listing.window)
-                device.list_files(destination, delete_listing)
-                device.delete_files(delete_listing)
-                delete_listing.show_result(ui)
-            # ui.replace_current(progress.window)
-            # progress.end(ui)
-    else:
-        raise Exception("Unknown Command !!")
+        device.push_files(progress, *file_listing.files)
+    progress.end(callback=progress_callback)
+    if args.delete:
+        delete_listing = DeleteList(get_source_path, device.should_delete)
+        dest_interface.list_files(destination, delete_listing)
+        dest_interface.delete_files(delete_listing)
+        delete_listing.show_result(ui)
